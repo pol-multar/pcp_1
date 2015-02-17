a86f2d64dba83ba1e60993fb3882c057e5011e12
Maxime MULTARI <maxime.multari@neuf.fr>
2015-02-17 11:42:10 +0100

    Ajout des constantes

---
+++ src/main/java/data/Constantes.java
@@ -0,0 +1,23 @@
+package data;
+
+/**
+ * @author Maxime
+ * @version 17/02/2015.
+ */
+public class Constantes {
+
+    //Conductivité thermique du matériau en W.m-1.K-1
+    public static final double lambdaLaineVerre=0.04;
+    public static final double lambdaBrique=0.84;
+    public static final double lambdaGranite=2.2;
+
+    //masse volumique du matériau en kg.m-3
+    public static final double muLaineVerre=30;
+    public static final double muBrique=1400;
+    public static final double muGranite=2700;
+
+    //chaleur spécifique massique du matériau en J.kg-1.K-1
+    public static final double cLaineVerre=900;
+    public static final double cBrique=840;
+    public static final double cGranite=790;
+}
---
+++ src/main/java/data/Wall.java
@@ -0,0 +1,8 @@
+package data;
+
+/**
+ * @author Maxime
+ * @version 03/02/2015.
+ */
+public class Wall {
+}
