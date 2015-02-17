f73262828f9529b95048a129ce3f2075c7c603e8
Maxime MULTARI <maxime.multari@neuf.fr>
2015-02-17 12:01:41 +0100

    Ajout de nouvelles constantes

--- src/main/java/data/Constantes.java
+++ src/main/java/data/Constantes.java
@@ -7,17 +7,25 @@ package data;
 public class Constantes {
 
     //Conductivité thermique du matériau en W.m-1.K-1
-    public static final double lambdaGlassWool=0.04;
-    public static final double lambdaBrick=0.84;
-    public static final double lambdaGranite=2.2;
+    public static final double GLASSWOOLLAMBDA=0.04;
+    public static final double BRICKLAMBDA=0.84;
+    public static final double GRANITELAMBDA=2.2;
 
     //masse volumique du matériau en kg.m-3
-    public static final double muGlassWool=30;
-    public static final double muBrick=1400;
-    public static final double muGranite=2700;
+    public static final double GLASSWOOLMU=30;
+    public static final double BRICKMU=1400;
+    public static final double GRANITEMU=2700;
 
     //chaleur spécifique massique du matériau en J.kg-1.K-1
-    public static final double cGlassWool=900;
-    public static final double cBrick=840;
-    public static final double cGranite=790;
+    public static final double GLASSWOOLC=900;
+    public static final double BRICKC=840;
+    public static final double GRANITEC=790;
+
+    //Temperatures en degré celsius
+    public static final double T0=20;
+    public static final double T1=55;
+
+    public static final double B=40;
+
+    public static final double OMEGA=86400/(2*Math.PI);
 }
--- src/main/java/data/Wall.java
+++ src/main/java/data/Wall.java
@@ -5,4 +5,11 @@ package data;
  * @version 03/02/2015.
  */
 public class Wall {
+
+    //Epaisseur de beton en centimetres
+    private static final int brickLength=32;
+    //Epaisseur de laine de verre en centimetres
+    public static final int glassWoolLength=12;
+
+
 }
