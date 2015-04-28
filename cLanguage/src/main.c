6d1b56becc34cac78154803d1ce5996290c0d5e1
BuildTools <unconfigured@null.spigotmc.org>
2015-04-28 10:53:39 +0200

    Implementation de l'algorithme monothread

--- cLanguage/src/main.c
+++ cLanguage/src/main.c
@@ -53,6 +53,9 @@
 	//Le numero de l'etape courante
      	int actualStep=0;
 
+    //Le temps total d'execution de l'algorithme
+    double execTime=0; 	
+
 //Variables pour le calcul de temps d'execution
 time_t debut, fin;
 float duree;
@@ -60,6 +63,10 @@ float duree;
 //some code
 //fin=time(NULL);
 //duree=difftime(fin,debut);
+
+//Pour savoir si la derniere couche a changee de temperature et a quelle etape
+int isChanged=0;
+int stepOfChange;
 
 
 	//Initialisation du mur
@@ -70,6 +77,48 @@ float duree;
 
      	currentTemp[0] = nextTemp[0] = OUTSIDETEMP;
      	currentTemp[8] = nextTemp[8] = INSIDETEMP;
+
+
+//Simulation en monothread
+for (int cpt = 0; cpt < maxStep; ++cpt)
+{
+	debut=time(NULL);
+
+        /**
+         * La premiere et la dernière partie du mur (respectivement partie 0 et partie 8) sont des constantes,
+         * on ne va donc pas modifier leur valeur de temperature
+         */
+
+        /* Etape 1 : modification des parties du mur composees du premier materiau, soit les parties 1 a 4 */
+        for (int i = 1; i < 5; i++) {
+            nextTemp[i] = currentTemp[i]+wallC*(currentTemp[i+1]+currentTemp[i-1]-2*(currentTemp[i]));
+        }
+
+        /* Etape 2 : modification de la temperature de la partie du milieu, soit la partie 5 */
+
+        nextTemp[5] = currentTemp[5] + wallC * (currentTemp[4] - currentTemp[5]) + insulationC * (currentTemp[6] - currentTemp[5]);
+
+        /* Etape 3 : modification de la temperature des dernieres parties du mur, soit les parties 6 et 7 */
+
+        for (int i = 6; i < 8; i++) {
+            nextTemp[i] = currentTemp[i]+insulationC*(currentTemp[i+1]+currentTemp[i-1]-2*(currentTemp[i]));
+        }
+
+        if (((int) nextTemp[7]) > 20 && isChanged==0) {
+            stepOfChange = actualStep;
+            isChanged = 1;
+        }
+
+        /*  Je met a jour les temperatures */
+        for (int j = 1; j<8; j++) {
+            currentTemp[j] = nextTemp[j];
+        }
+
+        //Le cycle est termine
+        actualStep++;
+        fin=time(NULL);
+        execTime += difftime(fin,debut);
+}
 
 
 
