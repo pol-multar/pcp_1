0cd5e2868e3807ae737112fa3cf15f666312c9a7
Maxime MULTARI <maxime.multari@neuf.fr>
2015-04-29 16:01:51 +0200

    creation des threads

--- cLanguage/src/main.c
+++ cLanguage/src/main.c
@@ -7,6 +7,11 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define QUEUESIZE 1
 
 /**
      * Methode permettant de calculer C,
@@ -16,46 +21,46 @@
      * @param c
      * @return un double contenant la valeur de C calculee
      */
-     float calculateC(float lambda, float mu, float c, float DT, float DX) {
+
+float calculateC(float lambda, float mu, float c, float DT, float DX) {
 
      	return (lambda * DT) / (mu * c * DX * DX);
 
      }
 
+void monoThreadSimulation(){
 
-     int main(int argc, char *argv[]){
+        //Temperatures en degré celsius
+        const float T0 = 20;
+        const float OUTSIDETEMP = 110;
+        const float INSIDETEMP = 20;
 
-	//Temperatures en degré celsius
-     	const float T0 = 20;
-     	const float OUTSIDETEMP = 110;
-     	const float INSIDETEMP = 20;
-
- 	//Pas de temps (secondes)
-     	const float DT=600;
+    //Pas de temps (secondes)
+        const float DT=600;
 
     //Pas d'espace (m)
-     	const float DX=0.04;
+        const float DX=0.04;
 
-	//Le tableau contenant les valeurs actuelles des couches
-     	float currentTemp[9];
+    //Le tableau contenant les valeurs actuelles des couches
+        float currentTemp[9];
 
-	//Le tableau contenant les valeurs prochaines des couches
-     	float nextTemp[9];
+    //Le tableau contenant les valeurs prochaines des couches
+        float nextTemp[9];
 
-	//Le C du materiau composant le mur
-     	const float wallC=calculateC(0.84, 1400, 840, DT, DX);
+    //Le C du materiau composant le mur
+        const float wallC=calculateC(0.84, 1400, 840, DT, DX);
 
-	//Le C du materiau composant l'isolant
-     	const float insulationC=calculateC(0.04, 30, 900, DT, DX);
+    //Le C du materiau composant l'isolant
+        const float insulationC=calculateC(0.04, 30, 900, DT, DX);
 
-	//Le nombre d'etapes de la simulation
-     	int maxStep=100000;
+    //Le nombre d'etapes de la simulation
+        int maxStep=100000;
 
-	//Le numero de l'etape courante
-     	int actualStep=0;
+    //Le numero de l'etape courante
+        int actualStep=0;
 
     //Le temps total d'execution de l'algorithme
-        double execTime; 	
+        double execTime;    
 
 //Variables pour le calcul de temps d'execution
         clock_t debut, fin;
@@ -66,19 +71,19 @@
         int stepOfChange;
 
 
-	//Initialisation du mur
+    //Initialisation du mur
         for (int i = 1; i < 8; ++i)
         {
-         currentTemp[i]=nextTemp[i]=T0;		
-     }
+           currentTemp[i]=nextTemp[i]=T0;     
+       }
 
-     currentTemp[0] = nextTemp[0] = OUTSIDETEMP;
-     currentTemp[8] = nextTemp[8] = INSIDETEMP;
+       currentTemp[0] = nextTemp[0] = OUTSIDETEMP;
+       currentTemp[8] = nextTemp[8] = INSIDETEMP;
 
      //Affichage de l'etat du mur avant simulation
-     printf("t=%d heure(s)",0 );
+       printf("t=%d heure(s)",0 );
 
-     for (int i = 0; i < 9; i++) {
+       for (int i = 0; i < 9; i++) {
         printf("%d,",(int)currentTemp[i]);
     }
 
@@ -91,7 +96,7 @@
     //Simulation en monothread
     for (int cpt = 0; cpt < maxStep; ++cpt)
     {
-       debut=clock();
+     debut=clock();
 
         /**
          * La premiere et la dernière partie du mur (respectivement partie 0 et partie 8) sont des constantes,
@@ -160,9 +165,258 @@
 
     }
 
+}
+
+void *producer (void *args);
+void *consumer (void *args);
+
+typedef struct {
+    int buf[QUEUESIZE];
+    long head, tail;
+    int full, empty;
+    pthread_mutex_t *mut;
+    pthread_cond_t *notFull, *notEmpty;
+} queue;
 
 
+//Pour que deux couches puissent communiquer, elles doivent partager deux files
 
+
+queue *queueInit (void);
+void queueDelete (queue *q);
+void queueAdd (queue *q, int in);
+void queueDel (queue *q, int *out);
+
+void* layerEngine(void *nb);
+
+queue* com[9][2];
+
+void multiThreadSimulation(){
+
+//J'initialise le tableau contenant les communication entre threads
+for (int i = 0; i < 9; ++i)
+    {
+        com[i][0]=queueInit();
+        com[i][1]=queueInit();
+        if (com[i][0] ==  NULL || com[i][1] ==  NULL ) {
+            fprintf (stderr, "main: Queue Init failed.\n");
+            exit (1);
+        }
+    }
+
+pthread_t c1,c2,c3,c4,c5,c6,c7;
+
+int nb1=1;
+int nb2=2;
+int nb3=3;
+int nb4=4;
+int nb5=5;
+int nb6=6;
+int nb7=7;
+
+//queue *fifo;
+//pthread_t pro, con;
+
+//fifo = queueInit ();
+/*
+if (fifo ==  NULL) {
+    fprintf (stderr, "main: Queue Init failed.\n");
+    exit (1);
+}*/
+
+
+if(pthread_create (&c1, NULL, layerEngine, &nb1)==-1){
+    perror("thread 1");
+    exit(1);
+}
+if(pthread_create (&c2, NULL, layerEngine, &nb2)==-1){
+        perror("thread 2");
+    exit(1);
+}
+if(pthread_create (&c3, NULL, layerEngine, &nb3)==-1){
+        perror("thread 3");
+    exit(1);
+}
+if(pthread_create (&c4, NULL, layerEngine, &nb4)==-1){
+        perror("thread 4");
+    exit(1);
+}
+if(pthread_create (&c5, NULL, layerEngine, &nb5)==-1){
+        perror("thread 5");
+    exit(1);
+}
+if(pthread_create (&c6, NULL, layerEngine, &nb6)==-1){
+        perror("thread 6");
+    exit(1);
+}
+if(pthread_create (&c7, NULL, layerEngine, &nb7)==-1){
+        perror("thread 7");
+    exit(1);
+}
+
+//pthread_create (&pro, NULL, producer, fifo);
+//pthread_create (&con, NULL, consumer, fifo);
+pthread_join (c1, NULL); printf("Fin de la thread c1\n");
+pthread_join (c2, NULL); printf("Fin de la thread c2\n");
+pthread_join (c3, NULL); printf("Fin de la thread c3\n");
+pthread_join (c4, NULL); printf("Fin de la thread c4\n");
+pthread_join (c5, NULL); printf("Fin de la thread c5\n");
+pthread_join (c6, NULL); printf("Fin de la thread c6\n");
+pthread_join (c7, NULL); printf("Fin de la thread c7\n");
+//queueDelete (fifo);
+for (int i = 0; i < 9; ++i)
+    {
+        queueDelete(com[i][0]);
+        queueDelete(com[i][1]);
+    }
+
+
+}
+
+void *layerEngine(void *nb)
+{
+int number;
+
+number=*(int*)nb;
+
+printf("Je suis la couche %d\n",number);
+
+return (NULL);
+}
+/*
+void *producer (void *q)
+{
+    queue *fifo;
+    int i;
+
+    fifo = (queue *)q;
+
+    for (i = 0; i < LOOP; i++) {
+        pthread_mutex_lock (fifo->mut);
+        while (fifo->full) {
+            printf ("producer: queue FULL.\n");
+            pthread_cond_wait (fifo->notFull, fifo->mut);
+        }
+        queueAdd (fifo, i);
+        pthread_mutex_unlock (fifo->mut);
+        pthread_cond_signal (fifo->notEmpty);
+        usleep (100000);
+    }
+    for (i = 0; i < LOOP; i++) {
+        pthread_mutex_lock (fifo->mut);
+        while (fifo->full) {
+            printf ("producer: queue FULL.\n");
+            pthread_cond_wait (fifo->notFull, fifo->mut);
+        }
+        queueAdd (fifo, i);
+        pthread_mutex_unlock (fifo->mut);
+        pthread_cond_signal (fifo->notEmpty);
+        usleep (200000);
+    }
+    return (NULL);
+}
+
+void *consumer (void *q)
+{
+    queue *fifo;
+    int i, d;
+
+    fifo = (queue *)q;
+
+    for (i = 0; i < LOOP; i++) {
+        pthread_mutex_lock (fifo->mut);
+        while (fifo->empty) {
+            printf ("consumer: queue EMPTY.\n");
+            pthread_cond_wait (fifo->notEmpty, fifo->mut);
+        }
+        queueDel (fifo, &d);
+        pthread_mutex_unlock (fifo->mut);
+        pthread_cond_signal (fifo->notFull);
+        printf ("consumer: recieved %d.\n", d);
+        usleep(200000);
+    }
+    for (i = 0; i < LOOP; i++) {
+        pthread_mutex_lock (fifo->mut);
+        while (fifo->empty) {
+            printf ("consumer: queue EMPTY.\n");
+            pthread_cond_wait (fifo->notEmpty, fifo->mut);
+        }
+        queueDel (fifo, &d);
+        pthread_mutex_unlock (fifo->mut);
+        pthread_cond_signal (fifo->notFull);
+        printf ("consumer: recieved %d.\n", d);
+        usleep (50000);
+    }
+    return (NULL);
+}
+*/
+
+queue *queueInit (void)
+{
+    queue *q;
+
+    q = (queue *)malloc (sizeof (queue));
+    if (q == NULL) return (NULL);
+
+    q->empty = 1;
+    q->full = 0;
+    q->head = 0;
+    q->tail = 0;
+    q->mut = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+    pthread_mutex_init (q->mut, NULL);
+    q->notFull = (pthread_cond_t *) malloc (sizeof (pthread_cond_t));
+    pthread_cond_init (q->notFull, NULL);
+    q->notEmpty = (pthread_cond_t *) malloc (sizeof (pthread_cond_t));
+    pthread_cond_init (q->notEmpty, NULL);
+    
+    return (q);
+}
+
+
+void queueDelete (queue *q)
+{
+    pthread_mutex_destroy (q->mut);
+    free (q->mut);  
+    pthread_cond_destroy (q->notFull);
+    free (q->notFull);
+    pthread_cond_destroy (q->notEmpty);
+    free (q->notEmpty);
+    free (q);
+}
+
+void queueAdd (queue *q, int in)
+{
+    q->buf[q->tail] = in;
+    q->tail++;
+    if (q->tail == QUEUESIZE)
+        q->tail = 0;
+    if (q->tail == q->head)
+        q->full = 1;
+    q->empty = 0;
+
+    return;
+}
+
+void queueDel (queue *q, int *out)
+{
+    *out = q->buf[q->head];
+
+    q->head++;
+    if (q->head == QUEUESIZE)
+        q->head = 0;
+    if (q->head == q->tail)
+        q->empty = 1;
+    q->full = 0;
+
+    return;
+}
+
+
+int main(int argc, char *argv[]){
+
+	
+    //monoThreadSimulation();
+    multiThreadSimulation();
 
 
     return 0;
